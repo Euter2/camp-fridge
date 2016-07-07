0a3438332eeeb7f755a4f0785a44bbcd173b6a45
Euter2 <filipmar.mf@gmail.com>
2016-07-07 17:22:07 +0200

    Add files via upload

---
+++ camp-fridge.ino
@@ -0,0 +1,64 @@
+#include <Timer.h>
+//#include <OneWire.h>
+//#include <DallasTemperature.h>
+
+//temperature related variables
+const float hysteresis = 3.0; //<-- setup required!!!
+const float targetTemp = 5.0; //<-- setup required!!!
+const long updatePeriod = 1*1000; //<-- setup required!!!
+const int tempPin = 4;
+const int heatPin = 13;
+const int hornPin = 6;
+const int sensorPin = A0; //TEST
+int value = 0;
+float temp = 0.0;
+//const float offset = -4.0; //<-- setup required!!!
+//OneWire oneWire(tempPin);
+//DallasTemperature sensor(&oneWire);
+
+
+
+// timer
+Timer t;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(heatPin, OUTPUT);
+  t.every(updatePeriod, measureTemp);
+  t.every(updatePeriod, checkTemp);
+
+}
+
+void loop() {  
+t.update();
+}
+
+void measureTemp() {
+  //read value from temp sensor with calibration offset
+  //sensor.requestTemperaturesByIndex(0);
+  //temp = sensor.getTempCByIndex(0) + offset;
+  value = analogRead(sensorPin);
+  temp = map(value,0,1023,-10,10);
+  }
+
+void checkTemp() {
+  if (temp<targetTemp-(hysteresis/2)) {
+    digitalWrite(heatPin, HIGH);
+    digitalWrite(hornPin, LOW);
+    Serial.print("LOW Temperature: ");
+    Serial.println(temp);
+    } else if (temp>targetTemp+(hysteresis/2)) {
+      digitalWrite(hornPin, HIGH);
+      digitalWrite(heatPin, LOW);
+      Serial.print("HIGH Temperature: ");
+      Serial.print(temp);
+      Serial.println(" ALARM! ALARM! ALARM!");
+      
+      } else {
+        digitalWrite(heatPin, LOW);
+        digitalWrite(hornPin, LOW);
+        Serial.print("IDEAL Temperature: ");
+        Serial.println(temp);
+        }
+  } 
+  
